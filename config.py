48ca9bd510aa2ad6ab842420ef88097efa5c4fb0
Hou linbao <2939827325@qq.com>
2024-10-23 16:54:51 +0800

    modified:   config.py

--- config.py
+++ config.py
@@ -1,16 +1,14 @@
-smtp_server = 'smtp.163.com'
+smtp_server = 'smtp.example.com'
 smtp_port = 25
 
-sender_email = '15239288661@163.com'
-sender_password = 'VHeaV389jFjd3cMe'
+sender_email = 'sender@example.com'
+sender_password = 'password'
 
-recipient_email = '2939827325@qq.com'
+recipient_email = 'recipient@example.com'
 send_subject = 'Sydney'
 send_name = 'Sydney'
 
-target_sender = '2939827325@qq.com'
+target_sender = 'target@example.com'
 
-API_KEY = 'sk-tOTXaQwfZdAOaVwInPSLeF5lv1Iv1i5hpmpJtILqDgNuVziL'
-#备用
-#API_KEY = 'sk-JmFlLWM2smg4WVgXPMsZW38fHn6ai5HrNV0lXLH4gvOhxPZE'
-BASE_URL = 'https://xdaicn.top/v1'
+API_KEY = 'your_api_key'
+BASE_URL = 'https://example.com/v1'
